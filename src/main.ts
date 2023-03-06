79b280543a8da7c7300015c6dc20a35587f0cc82
NPCDW <chenyongli0520@gmail.com>
2023-03-06 20:38:37 +0800

    pinia

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,18 @@ import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './style.css'
 import App from './App.vue'
-import { get_config } from './store/config'
+import { useConfig } from './store/config'
 import { registerDefault } from './util/global-shortcut'
 import router from './router/router'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
+const pinia = createPinia()
 
+app.use(pinia)
 app.use(router)
 app.mount('#app')
 
 await registerDefault();
 
-await get_config();
+await useConfig().refresh_config()
--- src/store/config.ts
+++ src/store/config.ts
@@ -1,12 +1,88 @@
 import { invoke } from '@tauri-apps/api/tauri'
+import { defineStore } from 'pinia'
+import {ref, computed} from "vue";
 
-export let config: object | null;
+export const useConfig = defineStore('config', () => {
+    const config = ref<Config>()
 
-export async function get_config(): Promise<object> {
-    if (config) {
-        return config;
+    async function refresh_config() {
+        config.value = await invoke("get_config");
+        console.log("refresh_config ", config)
     }
-    config = await invoke("get_config");
-    console.log("config", config)
-    return config!;
+
+    const value = computed(async () => {
+        if (!config.value) {
+            await refresh_config()
+        }
+        return config.value
+    })
+
+    return { refresh_config, value }
+})
+
+interface CommonConfig {
+    word_selection_interval: number,
+    language: string,
+}
+
+interface BaiduCloudOcrConfig {
+    access_token: string,
+    access_token_expires_time: string,
+    client_id: string,
+    client_secret: string,
+}
+
+interface TencentCloudOcrConfig {
+    secret_id: string,
+    secret_key: string,
+}
+
+interface SpaceOcrOcrConfig {
+    api_key: string,
+}
+
+interface OcrConfig {
+    default_ocr_provide: string,
+    default_ocr_type: string,
+    default_ocr_language: string,
+    baidu_cloud: BaiduCloudOcrConfig,
+    tencent_cloud: TencentCloudOcrConfig,
+    space_ocr: SpaceOcrOcrConfig,
+}
+
+interface BaiduAITranslateConfig {
+    app_id: string,
+    app_secret: string,
+}
+
+interface TencentCloudTranslateConfig {
+    secret_id: string,
+    secret_key: string,
+}
+
+interface TranslateConfig {
+    default_translate_provide: string,
+    default_translate_source_language: string,
+    default_translate_target_language: string,
+    baidu_ai: BaiduAITranslateConfig,
+    tencent_cloud: TencentCloudTranslateConfig,
+}
+
+interface HotKeysDetail {
+    modifiers: number,
+    key: number,
+    text: string,
+}
+
+interface HotKeysConfig {
+    ocr: HotKeysDetail,
+    word_selection_translate: HotKeysDetail,
+    screenshot_translate: HotKeysDetail,
+}
+
+interface Config {
+    common: CommonConfig,
+    ocr: OcrConfig,
+    translate: TranslateConfig,
+    hot_keys: HotKeysConfig,
 }
