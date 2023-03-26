64b2c6a391d85427a13ea9c153022d26893c94f0
NPCDW <chenyongli0520@gmail.com>
2023-03-26 16:01:26 +0800

    截图页面去框架

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+// import { createI18n } from 'vue-i18n'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import './style.css'
@@ -16,5 +17,10 @@ app.use(router)
 
 await useConfig().refresh_config()
 await useRuntimeConfig().refreshRuntimeConfig()
+
+// const i18n = createI18n({
+//     // something vue-i18n options here ...
+// })
+// app.use(i18n)
 
 app.mount('#app')
